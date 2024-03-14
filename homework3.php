73262a58cceb50b1712b4e2fc366c04475664601
Duncan0804 <126178274+Duncan0804@users.noreply.github.com>
2024-03-14 09:21:53 +0800

    week3

---
+++ homework3.php
@@ -0,0 +1,47 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="林宇濬的履歷表">
+    <title>林宇濬的履歷表</title>
+</head>
+
+<body  background="background.jpg">
+    <center><img src="123.jpg" width="200" height="300" alt="照片" /><font size="35" color="black"><b>林宇濬的履歷表</b></font></center>
+    <hr color="black" width="100%">
+    <h1>自我介紹</h1>
+    <ul>
+        <li><b>姓名</b>：林宇濬</li>
+        <li><b>學校</b>：國立高雄大學</li>
+        <li><b>科系</b>：資訊管理學系</li>
+        <li><b>興趣</b>：打籃球、打遊戲</li>
+        <li><b>MBTI</b>：INFP</li>
+    </ul>
+    <br/>
+    <hr color="black" width="20%">
+    <h2>個人資料</h2>
+    <ul>
+        <li><b>生日：</b>2004/02/01</li>
+        <li><b>電話：</b>09xxxxxxxx</li>
+        <li><b>信箱：</b>a1113355@gmail.com</li>
+        <li><b>Instagram：</b><a href="https://www.instagram.com/yu_jun_lin?igsh=MWM3Nzg2aXhpYXZpNQ%3D%3D&utm_source=qr">yu_jun_lin</a></li>
+    </ul>
+    <hr color="black" width="20%">
+    <h2>工作經歷</h2>
+    <table border="1">
+        <caption>工作經歷表</caption>
+        <thead>
+            <tr>
+                <th>公司名稱</th>
+                <th>工作職稱</th>
+                <th>工作期間</th>
+                <th>工作內容</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- 这里放你的工作经历 -->
+        </tbody>
+    </table>
+</body>
+
+</html>
